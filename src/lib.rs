b0a611f9c49cee217ee21b37d6a7b98b70646207
yyyz <904108079@qq.com>
2023-11-23 10:43:47 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 // use libc::c_char;
 // use core::ffi::CStr;
 // use ndarray::{array, Array2};
-
+#![no_std]
+use panic_halt as _;
 #[no_mangle]
 pub extern "C" fn test() -> i32 {
     3
--- test.c
+++ test.c
@@ -1,15 +1,14 @@
-#include <stdio.h>
+// #include <stdio.h>
 #include <stdint.h>
-#include <inttypes.h>
+// #include <inttypes.h>
 
 
-extern int32_t
-test();
+extern int test();
 
 int main(void) {
 
 
-  int32_t pop1 = test();
+  int pop1 = test();
 
-  printf("%" PRId32 "\n", pop1);
+  printf("%d \n", pop1);
 }
